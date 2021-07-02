92f0730c231e27630b4062d8414839a9abc0ccfc
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-07-01 23:13:00 -0600

    add infomation on reverse() and sort() shorthand

--- data-structures/array.js
+++ data-structures/array.js
@@ -1,40 +1,13 @@
-// Define a comparison function that will be used
-// to sort the array in ascending order
-function compareAscending(a, b) {
-  // if a > b, return 1
-  // The sort() method will flip the order of a and b
-  if (a > b) {
-    return 1;
-    // Do not flip the order of a and b otherwise
-  } else {
-    return -1;
-  }
-}
+let myArray = [1, 2, 3, 4, 5];
+console.log(myArray); // [1, 2, 3, 4, 5]
 
-// Define a comparison function that will be used
-// to sort the array in descending order
-function compareDescending(a, b) {
-  // if a < b, return 1
-  // The sort() method will flip the order of a and b
-  if (a < b) {
-    return 1;
-    // Do not flip the order of a and b otherwise
-  } else {
-    return -1;
-  }
-}
+// Reverse myArray
+myArray.reverse();
+console.log(myArray);
 
-// Define an array with unsorted numbers
-let unsorted = [5, 1, 7, 3, 10, 9, 2, 4, 6, 8];
+let myArray2 = ["Hi", "my", "name", "is", "joe"];
+console.log(myArray2); // ["Hi", "my", "name", "is", "joe"]
 
-// Test the sort() method without the use of comparison function
-console.log("Using sort() method:");
-console.log(unsorted.sort());
-
-// Test the sort() method using the compareAscending function
-console.log("Using sort() method with compareAscending:");
-console.log(unsorted.sort((a, b) => compareAscending(a, b)));
-
-// Test the sort() method using the compareDescending function
-console.log("Using sort() method with compareDescending:");
-console.log(unsorted.sort((a, b) => compareDescending(a, b)));
+// Reverse myArray2
+myArray2.reverse();
+console.log(myArray2);
